7f65ffb524aac4665cd89f46127e73057aed4f99
Lindsay Lambert <LindsayLambo@fullsail.edu>
2015-02-13 18:52:15 -0500

    Working on this accordion!!

--- Lambert_Lindsay_Project/js/main.js
+++ Lambert_Lindsay_Project/js/main.js
@@ -1,12 +1,44 @@
 /*  
-	Your Project Title
-	Author: You
+	PC Pet Charities and Projects
+	Author: Lindsay Lambert
 */
 
 (function($){
 	
 	
+  $('#tab-acc p').hide().eq(0).show();
+  $('#tab-acc p:not(:first)').hide();
+  $('.tabs li').click(function(e) {
+	  e.preventDefault();
+	  $('#tab-acc p').hide();
+  	  
+	  $('.tabs .current').removeClass("current");
+	  $(this).addClass('current');
+	  var clicked = $(this).find('a:first').attr('href');
+	  $('#tab-acc ' + clicked).fadeIn(3000);
+ 	  }).eq(0).addClass('current');
+ 	
+ 
 	
+	
+	
+	
+	
+	
+	/*$("#fadein").click(function() {
+                $("#theDiv").fadeIn("normal");
+            });
+            $("#fadeout").click(function() {
+                $("#theDiv").fadeOut("normal");
+            });
+            $("#fadeto3").click(function() {
+                $("#theDiv").fadeTo("slow", 0.3);
+            });
+            $("#fadeup").click(function() {
+                $("#theDiv").fadeTo("slow", 1.0);
+            });
+
+	*/
 	
 		
 
