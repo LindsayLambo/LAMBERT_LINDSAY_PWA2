ed903ff45a046e720dfbd954dc3ac2be000bed42
Lindsay Lambert <LindsayLambo@fullsail.edu>
2015-02-13 22:11:47 -0500

    So much code that doesn't work!

--- Lambert_Lindsay_Project/js/main.js
+++ Lambert_Lindsay_Project/js/main.js
@@ -5,7 +5,9 @@
 
 (function($){
 	
-	
+/* Accordion  ----------------------------------------- */	
+ 
+ 
   $('#tab-acc p').hide().eq(0).show();
   $('#tab-acc p:not(:first)').hide();
   $('.tabs li').click(function(e) {
@@ -18,11 +20,45 @@
 	  $('#tab-acc ' + clicked).fadeIn(3000);
  	  }).eq(0).addClass('current');
  	
- 
+
+/*  Modal  ----------------------------------------- */
+
+
+	$('.clickModal').on('click', function(event) {
+		event.preventDefault();
+		$('#overlay').fadeIn().find('#modal').fadeIn();
+	});
+	
+	$('.x').on('click', function(event) {
+		event.preventDefault();
+		$('#overlay').fadeOut().find('#modal').fadeOut();
+	});
+	
+	$('.myStatus').mouseover(function() {
+		$(this).fadeTo(100, .3);
+	});
+	
+	$('.myStatus').mouseout(function() {
+		$(this).fadeTo(100, 1);
+	});
 	
 	
+/*  Tooltips  ----------------------------------------- */	
 	
 	
+	$('.masterTooltip').hover(function() {
+		var title = $(this).attr('title');
+		$(this).data('tipText', title).removeAttr('title');
+		$('<p class="tooltip"></p>').text(title).appendTo('body').fadeIn('slow');
+	},  function() {
+		$(this).attr('title', $(this).data('tipText'));
+		$('.tooltip').remove();
+	}).mousemove(function(e) {
+		var mousex = e.pagex + 20;
+		var mousey = e.pagey + 10;
+		$('.tooltip').css({ top: mousey, left: mousex })
+});
+		
 	
 	
 	/*$("#fadein").click(function() {
